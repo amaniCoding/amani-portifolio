2688e09e4c1b5459e4ef9691fca2033a9ec97a6b
amaniCoding <amanuelfrm@gmail.com>
2024-12-22 17:42:57 +0300

    add features of modal and context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import "./globals.css";
 import { Entry } from "./ui/app";
-import { Children } from "react";
 export const metadata: Metadata = {
   title: 'Amanuel Ferede | Fullstack software engineer',
   description: 'Amanuel Ferede, a passion to create marvelous software and programming solutions to different real life problems. I have an intuitive desire to learn new technologies and how they work in depth.',
