d1cafe37075258d1465bd8225ab74e029ca78cc2
amaniCoding <amanuelfrm@gmail.com>
2024-12-30 12:01:06 +0300

    fixed some dark mode issues

--- app/ui/nav-bar.tsx
+++ app/ui/nav-bar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import clsx from "clsx";
 import Image from 'next/image';
 import Link from "next/link";
