b6da760f5d6bf719fd7e91570ddd740faaff01c7
amaniCoding <amanuelfrm@gmail.com>
2025-01-11 15:57:38 +0300

    'updated'

--- app/ui/hero-image.tsx
+++ app/ui/hero-image.tsx
@@ -7,14 +7,13 @@ export default function HeroImage() {
         e.preventDefault();
         window.scrollTo({
             // @ts-expect-error: Object is possibly 'null'.
-            top: document.querySelector("#contact-me").offsetTop-72,
+            top: document.querySelector("#contact-me").offsetTop - 72,
             behavior: "smooth"
         });
     }
     return (
         <section className="w-full dark:bg-black md:bg-slate-600 md:mt-[64px] mt-[72px]" id='home'>
             <div className='max-w-screen-xl mx-auto relative overflow-hidden'>
-
                 <div className='flex md:flex-row flex-col md:min-h-[99vh]'>
                     <div className='md:w-1/2 felx md:p-4 p-0 content-center'>
                         <div className='flex w-full flex-col space-y-1 md:space-y-4 p-4'>
@@ -22,10 +21,10 @@ export default function HeroImage() {
                             <p className='md:text-xl text-lg  md:text-white text-black w-full dark:text-slate-400'>A Full Stack Software Engineer ! <small> with a passion to create marvelous software and programming solutions to different real life problems. I have an intuitive desire to learn new technologies and how they work in depth.</small></p>
 
                             <div className='flex md:flex-col flex-row md:space-y-4 space-x-4'>
-                                <Link href={`#`} onClick={(e) => {handelScroll(e)}} className=" md:py-3 md:block py-2 rounded-md px-1 md:w-52 w-32 md:bg-orange-600 bg-transparent md:text-white ring-orange-600 border-2 md:border-white border-black text-center hidden dark:bg-black dark:text-white dark:hover:bg-slate-600 dark:hover:text-white hover:bg-orange-400">Contact Me</Link>
+                                <Link href={`#`} onClick={(e) => { handelScroll(e) }} className=" md:py-3 md:block py-2 rounded-md px-1 md:w-52 w-32 md:bg-orange-600 bg-transparent md:text-white ring-orange-600 border-2 md:border-white border-black text-center hidden dark:bg-black dark:text-white dark:hover:bg-slate-600 dark:hover:text-white hover:bg-orange-400">Contact Me</Link>
 
-                                <Link href={`#`} onClick={(e) => {handelScroll(e)}} className='md:hidden block'>
-                                <PhoneArrowDownLeftIcon className='w-11 h-11 p-3 bg-orange-500 rounded-full stroke-slate-50 dark:bg-slate-600 dark:text-white'/>
+                                <Link href={`#`} onClick={(e) => { handelScroll(e) }} className='md:hidden block'>
+                                    <PhoneArrowDownLeftIcon className='w-11 h-11 p-3 bg-orange-500 rounded-full stroke-slate-50 dark:bg-slate-600 dark:text-white' />
                                 </Link>
                                 <div className='flex flex-row space-x-4 items-center'>
                                     <div className="flex items-center justify-center bg-slate-50 rounded-full w-10 h-10">
@@ -58,14 +57,15 @@ export default function HeroImage() {
                                         <p className='p-2  md:text-white dark:text-white'>+251918634810</p>
                                     </div>
 
+                                    <div>
+                                        <Link href={`https://drive.google.com/file/d/1ohIyY6rr44uTVCIWJbe5SyhXh-8fQBGN/view?usp=drive_link`} target='_blank' className='p-2  md:text-white dark:text-white'>My CV</Link>
+                                    </div>
+
                                 </div>
                             </div>
 
                         </div>
-
-
                     </div>
-
                     <div className='md:w-1/2 w-full flex  justify-center items-center md:order-last order-first'>
                         <Image
                             src="/7.png"
@@ -78,13 +78,13 @@ export default function HeroImage() {
                         />
                     </div>
                     <Image
-                            src="/9.png"
-                            alt="next starter project"
-                            width={0}
-                            height={0}
-                            sizes='100vh'
-                            className="w-9/12 h-auto md:block hidden bg-center absolute top-1 -right-24 z-10 dark:brightness-75"
-                        />
+                        src="/9.png"
+                        alt="next starter project"
+                        width={0}
+                        height={0}
+                        sizes='100vh'
+                        className="w-9/12 h-auto md:block hidden bg-center absolute top-1 -right-24 z-10 dark:brightness-75"
+                    />
                 </div>
             </div>
         </section>
