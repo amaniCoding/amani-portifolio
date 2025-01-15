60c574fde9a480fb9178079076b272409f82afbf
amaniCoding <amanuelfrm@gmail.com>
2025-01-15 11:46:18 +0300

    reformatted code

--- app/ui/nav-bar.tsx
+++ app/ui/nav-bar.tsx
@@ -48,9 +48,9 @@ export default function Navbar() {
 
     
     return (
-        <nav className="bg-white shadow-md fixed w-full z-20 top-0 left-0 right-0 dark:bg-black px-5">
+        <nav className="bg-white shadow-md fixed w-full z-20 top-0 left-0 right-0 dark:bg-black">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 relative">
-                <Link href={`#`} onClick={(e) => { hadelScroll(e, "home") }} className="flex items-center space-x-3">
+                <Link href={`#`} onClick={(e) => { hadelScroll(e, "home") }} className="flex items-center space-x-3 md:pl-0 pl-5">
                     <Image
                         src="/99.png"
                         alt="Amanuel Ferede"
@@ -64,14 +64,14 @@ export default function Navbar() {
                 </Link>
 
                 <div className={
-                    clsx("w-full md:block md:w-auto md:static absolute top-12 right-0 left-0 z-10 dark:bg-black",
+                    clsx("w-full bg-orange-50 md:block md:w-auto md:static absolute top-16 right-0 left-0 z-10 dark:bg-black",
                         {
                             "hidden": showCollapse === false
                         }
 
                     )
                 }>
-                    <ul className="flex flex-col mt-4 border border-gray-100 dark:bg-black md:bg-white bg-orange-50 md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0 md:border-0 dark:md:bg-black">
+                    <ul className="flex flex-col mt-4 dark:bg-black md:bg-white md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0 md:border-0 dark:md:bg-black">
                         <li>
                             <Link href="#" onClick={(e) => { hadelScroll(e, "home") }} className="block py-2 md:px-3 px-5 text-whit rounded md:hover:bg-orange-700 dark:hover:bg-gray-600 md:hover:text-white hover:bg-slate-200 hover:text-black dark:text-white transition duration-500 ease-in-out" aria-current="page">Home</Link>
                         </li>
@@ -103,7 +103,7 @@ export default function Navbar() {
 
                 
        
-                <button onClick={handelCollapse} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-white" aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={handelCollapse} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-white mr-5" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
