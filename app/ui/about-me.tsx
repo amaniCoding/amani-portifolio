40aaae58a5661d4c6b04d192385e597770183f3b
amaniCoding <amanuelfrm@gmail.com>
2025-01-18 14:09:44 +0300

    hot reloading

--- app/ui/about-me.tsx
+++ app/ui/about-me.tsx
@@ -1,11 +1,11 @@
 'use client'
 export default function AboutMe() {
   return (
-    <section className="w-full md:py-4 p-2 bg-orange-700 dark:bg-slate-900" id="about-me">
+    <section className="w-full md:py-4 p-2 md:bg-orange-700 dark:bg-slate-900" id="about-me">
 
       <div className="mx-auto max-w-screen-md md:py-10 py-3">
-        <h1 className='md:text-2xl text-lg font-bold text-center mb-1 text-white dark:text-slate-400'>About Me</h1>
-        <p className="text-white md:text-center text-justify md:p-0 p-2 w-full dark:text-slate-300">A Full Stack Software Engineer with a passion to create marvelous software and programming solutions to different real life problems. I have an intuitive desire to learn new technologies and how they work in depth.</p>
+        <h1 className='md:text-2xl text-lg font-bold text-center mb-1 md:text-white text-black dark:text-slate-400'>About Me</h1>
+        <p className="md:text-white text-black md:text-center text-justify md:p-0 p-2 w-full dark:text-slate-300">A Full Stack Software Engineer with a passion to create marvelous software and programming solutions to different real life problems. I have an intuitive desire to learn new technologies and how they work in depth.</p>
       </div>
 
     </section>
