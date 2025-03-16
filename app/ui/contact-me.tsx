e06ff6a868fcece9f9fc2957efb9cbd3a064ee84
amaniCoding <amanuelfrm@gmail.com>
2025-03-16 17:07:19 +0300

    'upudated-ui'

--- app/ui/contact-me.tsx
+++ app/ui/contact-me.tsx
@@ -16,21 +16,21 @@ export default function ContactMe() {
                         <div className="my-2">
                             <label htmlFor="name" className="my-1 block dark:text-white">Your Name</label>
                             <div className="my-3">
-                                <input className="focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-white dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-black dark:text-white" placeholder="Your Name"></input>
+                                <input className="focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-gray-600 dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-white dark:text-white" placeholder="Your Name"></input>
                             </div>
                         </div>
 
                         <div className="my-2">
                             <label htmlFor="name" className="my-1 block dark:text-white">Your Email</label>
                             <div className="my-3">
-                                <input className=" focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-white dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-black dark:text-white" placeholder="Your Email you@example.com"></input>
+                                <input className=" focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-gray-600 dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-white dark:text-white" placeholder="Your Email you@example.com"></input>
                             </div>
                         </div>
 
                         <div className="my-2">
                             <label htmlFor="name" className="my-1 block dark:text-white">Message</label>
                             <div className="my-3">
-                                <textarea rows={5} className="focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-white dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-black dark:text-white" placeholder="Your Message"></textarea>
+                                <textarea rows={5} className="focus:border-blue-700 p-3 block w-full outline-none border-[1.9px] dark:border-2 dark:border-gray-600 dark:placeholder:text-gray-500 dark:focus:placeholder:text-white focus:border-[1.99px] transition-all durationb-500 ease-in-out border-black/20 rounded-xl placeholder:text-slate-400 dark:placeholder:text-white dark:bg-transparent dark:focus:border-white dark:text-white" placeholder="Your Message"></textarea>
                             </div>
                         </div>
                         <div className="my-2">
@@ -49,37 +49,37 @@ export default function ContactMe() {
                         <div className="grid md:grid-cols-5 grid-cols-3 gap-6">
                             <div className="flex flex-col items-center justify-center">
 
-                                <Link href={`https://web.facebook.com/amanuel.ferede.mekonnen`} className="bg-slate-50 w-14 h-14 rounded-full flex items-center justify-center hover:shadow-lg transition-all duration-300 ease-in-out" target="_blank"><FaFacebook className="w-full h-full m-2 text-blue-600 dark:text-gray-600 dark:hover:text-gray-800" /></Link>
+                                <Link href={`https://web.facebook.com/amanuel.ferede.mekonnen`} target="_blank"><FaFacebook className="h-12 w-12 hover:text-blue-800 transition-all duration-500 ease-in-out m-2 text-blue-600 dark:text-white dark:hover:text-gray-200" /></Link>
 
-                                <h2 className="mt-2 dark:text-white">Facebook</h2>
+                                <h2 className="mt-2 dark:text-white text-center">Facebook</h2>
                             </div>
 
                             <div className="flex flex-col items-center justify-center">
 
-                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} className="bg-slate-50 w-14 h-14 rounded-full flex items-center justify-center hover:shadow-lg transition-all duration-300 ease-in-out" target="_blank"><FaTelegram className="w-full h-full m-2 text-blue-600 dark:text-gray-600 dark:hover:text-gray-800" /></Link>
+                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} target="_blank"><FaTelegram className="h-12 w-12 hover:text-blue-800 transition-all duration-500 ease-in-out m-2 text-blue-600 dark:text-white dark:hover:text-gray-200" /></Link>
 
-                                <h2 className="mt-2 dark:text-white">Telegram</h2>
+                                <h2 className="mt-2 dark:text-white text-center">Telegram</h2>
                             </div>
 
                             <div className="flex flex-col items-center justify-center">
 
-                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} className="bg-slate-50 w-14 h-14 rounded-full flex items-center justify-center hover:shadow-lg transition-all duration-300 ease-in-out" target="_blank"><FaLinkedinIn className="w-full h-full m-2 text-blue-600 dark:text-gray-600 dark:hover:text-gray-800" /></Link>
+                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} target="_blank"><FaLinkedinIn className="h-12 w-12 hover:text-blue-800 transition-all duration-500 ease-in-out m-2 text-blue-600 dark:text-white dark:hover:text-gray-200" /></Link>
 
-                                <h2 className="mt-2 dark:text-white">Linkdein</h2>
+                                <h2 className="mt-2 dark:text-white text-center">Linkdein</h2>
                             </div>
 
                             <div className="flex flex-col items-center justify-center">
 
-                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} className="bg-slate-50 w-14 h-14 rounded-full flex items-center justify-center hover:shadow-lg transition-all duration-300 ease-in-out" target="_blank"><FaWhatsapp className="w-full h-full m-2 text-blue-600 dark:text-gray-600 dark:hover:text-gray-800" /></Link>
+                                <Link href={`https://t.me/+zVNZv5ldN9Q2ZjU5`} target="_blank"><FaWhatsapp className="h-12 w-12 hover:text-blue-800 transition-all duration-500 ease-in-out m-2 text-blue-600 dark:text-white dark:hover:text-gray-200" /></Link>
 
-                                <h2 className="mt-2 dark:text-white">Whatsup</h2>
+                                <h2 className="mt-2 dark:text-white text-center">Whatsup</h2>
                             </div>
 
                             <div className="flex flex-col items-center justify-center">
 
-                                <Link href={`https://drive.google.com/file/d/11Zg7h75GUix9JXe8XI-KyCV-RAjaWb2I/view`} className="bg-slate-50 w-14 h-14 rounded-full flex items-center justify-center hover:shadow-lg transition-all duration-300 ease-in-out" target="_blank"><CiViewTimeline className="w-full h-full m-2 text-blue-600 dark:text-gray-600 dark:hover:text-gray-800" /></Link>
+                                <Link href={`https://drive.google.com/file/d/11Zg7h75GUix9JXe8XI-KyCV-RAjaWb2I/view`} target="_blank"><CiViewTimeline className="h-12 w-12 hover:text-blue-800 transition-all duration-500 ease-in-out m-2 text-blue-600 dark:text-white dark:hover:text-gray-200" /></Link>
 
-                                <h2 className="mt-2 dark:text-white">CV</h2>
+                                <h2 className="mt-2 dark:text-white text-center">CV</h2>
                             </div>
                         </div>
 
